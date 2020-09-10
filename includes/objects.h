c96ba676dac4b53b243ad7938d8b1ecf6b1ee3b6
Sami Brugier <sabrugie@mi-r2-p6.s19.be>
2020-09-10 16:31:45 +0200

    other objs added in .h

--- includes/objects.h
+++ includes/objects.h
@@ -6,7 +6,7 @@
 /*   By: sabrugie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 09:07:06 by sabrugie          #+#    #+#             */
-/*   Updated: 2020/09/10 14:36:24 by sabrugie         ###   ########.fr       */
+/*   Updated: 2020/09/10 15:09:48 by sabrugie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,36 @@ typedef struct	s_sphere
 
 typedef struct	s_plane
 {
-	t_vec					pos;
-	t_vec					dir;
-	t_vec					rgb;
-	struct s_mat			*mat_ptr;
+	t_vec			pos;
+	t_vec			ori;
+	t_vec			rgb;
+	struct s_mat	*mat_ptr;
 }				t_pl;
+
+typedef struct	s_square
+{
+	t_vec			pos;
+	t_vec			ori;
+	float			size;
+	t_vec			rgb;
+}				t_sq;
+
+typedef struct	s_cylinder
+{
+	t_vec			pos;
+	t_vec			ori;
+	float			diam;
+	float			height;
+	t_vec			rgb;
+}				t_cy;
+
+typedef struct	s_triangle
+{
+	t_vec			point1;
+	t_vec			point2;
+	t_vec			point3;
+	t_vec			rgb;
+}				t_tr;
 
 typedef struct	s_light
 {
@@ -63,6 +88,7 @@ typedef struct	s_object_list
 
 t_bool			hit_any(t_obj *objs, t_hit *hit, t_hit_rec *rec);
 t_bool			hit_sphere(t_sp *sp, t_hit *hit, t_hit_rec *rec);
+t_bool			hit_plane(t_pl *pl, t_hit *hit, t_hit_rec *rec);
 t_obj			*new_obj(char type, void *obj, t_obj *next, t_func f);
 t_sp			*new_sphere(t_sp *dest, t_vec *v1, float radius);
 t_vec			*rand_unit_sp(t_vec *dest, t_xor *seed);
